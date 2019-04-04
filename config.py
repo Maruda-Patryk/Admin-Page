369bcbd07375dab3f074c2b3409f9874f755a492
Patryk <patrykmaruda@gmail.com>
2019-04-04 14:33:04 +0200

    Create dev branch

---
+++ config.py
@@ -0,0 +1,8 @@
+class Config(object):
+    pass
+
+class DevConfig(Config):
+    DEBUG = True
+
+class ProdConfig(Config):
+    pass
---
+++ manage.py
@@ -0,0 +1,8 @@
+import os 
+from webapp import db , migrate , create_app
+
+env = os.environ.get('WEBAPP_ENV' , 'dev')
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(app=app , db=db)
